161c52cdbba53e57cf5e649140524e2c0106bc0a
AMINA4823 <meenahmuhammad48@gmail.com>
2025-07-23 04:00:14 -0700

    created signin page

---
+++ app/dashboard/profile/page.js
@@ -0,0 +1,7 @@
+import { Main } from "next/document";
+
+export default function Profile () {
+    return (
+        <main>welcome to Profile page </main>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return(
+        <main className="bg-white shadow shadow-gray-300 w-full"> 
+        <div className="flex justify-between h-[50px] pt-3 px-5">
+            <p className="text- blue-500 font-bold text-2xl">BetaOrder</p>
+            <ul className=""> 
+
+            </ul>
+
+        </div>
+
+        </main>
+    )
+}
